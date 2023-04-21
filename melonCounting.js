0c2886fa4faa712e9f5802a4c303786591444adc
Meggin <megginsimon@gmail.com>
2023-04-21 02:03:29 +0000

    additional .reduce review

--- melonCounting.js
+++ melonCounting.js
@@ -28,56 +28,56 @@ function countMelons(melonList){
 console.log(countMelons(melonsToAdd));
 
 
-// const melonCounts = melonsToAdd.reduce((mCounts, melon) => {
-//     if (mCounts[melon]) {
-//       mCounts[melon] += 1;
-//     } else {
-//       mCounts[melon] = 1;
-//     }
-//     return mCounts;
-//   }, {});
+const melonCounts = melonsToAdd.reduce((mCounts, melon) => {
+    if (mCounts[melon]) {
+      mCounts[melon] += 1;
+    } else {
+      mCounts[melon] = 1;
+    }
+    return mCounts;
+  }, {});
   
-// console.log(melonCounts);
+console.log(melonCounts);
 
-// https://www.google.com/search?q=reduce+in+javascript&rlz=1C1CHBF_enUS917US917&source=lnms&tbm=vid&sa=X&ved=2ahUKEwiIwc2Lo7n-AhUrj4kEHTCdBH8Q0pQJegQIBRAC&biw=976&bih=796&dpr=1.1#fpstate=ive&vld=cid:f890be46,vid:g1C40tDP0Bk
+https://www.google.com/search?q=reduce+in+javascript&rlz=1C1CHBF_enUS917US917&source=lnms&tbm=vid&sa=X&ved=2ahUKEwiIwc2Lo7n-AhUrj4kEHTCdBH8Q0pQJegQIBRAC&biw=976&bih=796&dpr=1.1#fpstate=ive&vld=cid:f890be46,vid:g1C40tDP0Bk
 
 
-// const melonCounts = melonsToAdd.reduce((mCounts, melon) => {
-//     mCounts[melon] = (mCounts[melon] || 0) + 1;
+const melonCounts = melonsToAdd.reduce((mCounts, melon) => {
+    mCounts[melon] = (mCounts[melon] || 0) + 1;
   
-//     return mCounts;
-//   }, {});
+    return mCounts;
+  }, {});
   
-//   console.log(melonCounts);
+  console.log(melonCounts);
 
 
 
 
-// const myArray = [1,2,3,4];
+const myArray = [1,2,3,4];
 
-// const addToMe = myArray.reduce((adding, number) => {
-//     if(adding<4){
-//         adding += number;
-//     }else{
-//         console.log("too high");
-//     }
-//     return adding
-// }, 0);
+const addToMe = myArray.reduce((adding, number) => {
+    if(adding<4){
+        adding += number;
+    }else{
+        console.log("too high");
+    }
+    return adding
+}, 0);
 
-// console.log(addToMe);
+console.log(addToMe);
 
 
 
-// const megArray = ["loic", "meg", "noemie", "lynn"];
+const megArray = ["yukon", "meg", "noemie", "lynn"];
 
-// const uniqueLetters = megArray.reduce((letters, lettre) => {
-//     if (letters.includes(lettre[0])){
-//         console.log("in there already");
-//     }else{
-//         letters.push(lettre[0]);
-//     };
+const uniqueLetters = megArray.reduce((letters, lettre) => {
+    if (letters.includes(lettre[0])){
+        console.log("in there already");
+    }else{
+        letters.push(lettre[0]);
+    };
 
-//     return letters
-// }, []);
+    return letters
+}, []);
 
-// console.log(uniqueLetters)
+console.log(uniqueLetters)
